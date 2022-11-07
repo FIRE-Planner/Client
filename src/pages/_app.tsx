7af6ca87cfda3bde0ba1a2413751d2fbc659a110
ctd <qpflapffhs76@gmail.com>
2022-11-07 21:30:06 +0900

    💄 add AppLayout component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,13 @@
 import { Global } from '@emotion/react';
 import type { AppProps } from 'next/app';
 import { globalStyle } from '@styles/globalStyles';
+import AppLayout from '@layouts/AppLayout';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <AppLayout>
       <Global styles={globalStyle} />
       <Component {...pageProps} />
-    </>
+    </AppLayout>
   );
 }
