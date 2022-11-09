71fc3d68813363aad7dfdde266e6e66130f4c062
ctd <qpflapffhs76@gmail.com>
2022-11-09 15:06:12 +0900

    🔧 add eslint import/order config

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,6 +17,48 @@ module.exports = {
     ecmaVersion: 12,
     sourceType: 'module',
   },
+  settings: {
+    'import/resolver': {
+      typescript: {
+        directory: './src',
+      },
+    },
+    'import/parsers': { '@typescript-eslint/parser': ['.ts', '.tsx'] },
+  },
   plugins: ['react', '@typescript-eslint', 'prettier'],
-  rules: {},
+  rules: {
+    'react/react-in-jsx-scope': 0,
+    'react/jsx-filename-extension': [2, { extensions: ['.tsx'] }],
+    'react/function-component-definition': [
+      2,
+      { namedComponents: 'arrow-function' },
+    ],
+    'react/jsx-props-no-spreading': 'off',
+    'react/prop-types': 'off',
+    'import/order': [
+      'error',
+      {
+        groups: ['builtin', 'external', 'parent', 'sibling', 'index'],
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+        'newlines-between': 'always',
+      },
+    ],
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        ts: 'never',
+        jsx: 'never',
+        tsx: 'never',
+      },
+    ],
+    'no-unused-vars': 'warn',
+    'react/no-unknown-property': ['error', { ignore: ['css'] }],
+    'import/prefer-default-export': 'off',
+    'no-use-before-define': 'off',
+  },
 };
